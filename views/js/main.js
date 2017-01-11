909815e0030abbec2fc4f065e94a4b10b3b484bb
Carlos P Navarrete Filho <karnav@gmail.com>
2017-01-11 16:30:38 -0200

    Optimization: Keep fps at 60 when scrolling.

--- views/js/main.js
+++ views/js/main.js
@@ -497,13 +497,16 @@ function logAverageFrame(times) {   // times is the array of User Timing measure
 // https://www.igvita.com/slides/2012/devtools-tips-and-tricks/jank-demo.html
 
 // Moves the sliding background pizzas based on scroll position
+
+var scrollTop = (document.body.scrollTop / 1250);
+
 function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
+  var items = document.querySelectorAll('.mover')
 
-  var items = document.querySelectorAll('.mover');
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(scrollTop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
